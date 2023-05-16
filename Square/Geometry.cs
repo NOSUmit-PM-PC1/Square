4dfc637e6d73cbb053d41f3bf7f383dbed8e084b
Мария <mahadrum@yandex.ru>
2023-05-16 11:34:42 +0000

    Добавьте файлы проекта.

---
+++ Square/Geometry.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square
+{
+    class Geometry
+    {
+
+    }
+
+    class Point
+    {
+        public int x, y;
+        public Point(int userX, int userY)
+        {
+            x = userX;
+            y = userY;
+        }
+
+        public string ToString()
+        { 
+            return  $"({x},{y})";       
+        }
+    }
+
+    class Vector
+    {
+        Point pointBegin, pointEnd;
+        Point koor;
+        public Vector(Point userPointBegin, Point userPointEnd)
+        {
+            pointBegin = userPointBegin;
+            pointEnd = userPointEnd;
+            koor = new Point(pointEnd.x - pointBegin.x, pointEnd.y - pointBegin.y);
+        }
+
+        public string ToString()
+        {
+            return $"({koor.x};{koor.y})";
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(koor.x * koor.x + koor.y * koor.y);
+        }
+        //псевдо векторное или косое векторное произведение
+        public int CroossProdact(Vector b)
+        {
+            return this.koor.x * b.koor.y - this.koor.y * b.koor.x;
+        }
+    }
+
+    class Triangle
+    {
+        Point A, B, C;
+        public Triangle(Point userA, Point userB, Point userC)
+        {
+            A = userA; B = userB; C = userC;
+        }
+
+        public double Square()
+        {
+            Vector ab = new Vector(A, B);
+            Vector ac = new Vector(A, C);
+            return Math.Abs(ab.CroossProdact(ac)) / 2;
+        }
+
+    }
+}
---
+++ Square/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(5, 6);
+            Point b = new Point(10, 1);
+            Point c = new Point(1, -10);
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(b.ToString());
+
+            Vector ab = new Vector(a, b);
+            Console.WriteLine(ab.ToString());
+            Console.WriteLine(ab.Length());
+
+            Triangle abc = new Triangle(a, b, c);
+            Console.WriteLine(abc.Square());
+
+            Triangle bac = new Triangle(c, a, b);
+            Console.WriteLine(bac.Square());
+
+            Triangle newTri = new Triangle(new Point(0, 0), new Point(0, 4), new Point(5, 0));
+            Console.WriteLine(newTri.Square());
+        }
+    }
+}
